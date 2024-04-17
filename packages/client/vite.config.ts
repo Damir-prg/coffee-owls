3cb1d853ebbdea45802c4409cd3ae011ec390375
Damir Nasirov <krekki01@mail.ru>
2024-04-17 03:34:34 +0300

    feat: Добавлены алиасы для файловой структуры FSD

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -11,5 +12,5 @@ export default defineConfig({
   define: {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
 })
