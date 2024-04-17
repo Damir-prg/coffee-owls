ddb0cab56b2922c450bf47d5ccb33c21ddb25d6c
Damir Nasirov <krekki01@mail.ru>
2024-04-17 05:15:22 +0300

    fix: semi в корне client

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import dotenv from 'dotenv'
-dotenv.config()
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import dotenv from 'dotenv';
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,4 +13,4 @@ export default defineConfig({
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
   plugins: [react(), tsconfigPaths()],
-})
+});
