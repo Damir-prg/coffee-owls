024f643e0bd02f9e17b3901458f6ddcf00ce7931
VeronicaGrechko <vdgrechko@yandex.ru>
2024-04-23 09:13:05 +0300

    fix: add zero margin to all titles

--- packages/client/src/app/App.tsx
+++ packages/client/src/app/App.tsx
@@ -27,6 +27,10 @@ const appLightTheme = {
       contentLineHeight: 1.5,
       contentFontSizeLG: 24,
     },
+    Typography: {
+      titleMarginBottom: 0,
+      titleMarginTop: 0,
+    },
   },
 };
 
