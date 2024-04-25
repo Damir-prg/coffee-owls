230b665fab02a32f7eee941225d26246b496f374
Damir Nasirov <krekki01@mail.ru>
2024-04-25 22:52:33 +0300

    refactor: moved logger of selected game screen into ScreenSwitcher

--- packages/client/src/pages/Game/ui/ScreenSwitcher.tsx
+++ packages/client/src/pages/Game/ui/ScreenSwitcher.tsx
@@ -1,8 +1,14 @@
-import { GameContextConsumer } from 'entities/GameContext';
+import { GameContextConsumer, GameContextInstance } from 'entities/GameContext';
 import { GameScreens } from '../lib/GameScreens';
 import { GameContentContainer } from 'entities/GameContentContainer';
+import { useContext, useEffect } from 'react';
 
 export const ScreenSwitcher = () => {
+  const { gameScreen } = useContext(GameContextInstance);
+
+  useEffect(() => {
+    console.log(`Активная страница игры: ${gameScreen}`);
+  }, [gameScreen]);
   return (
     <GameContentContainer>
       <GameContextConsumer>{({ gameScreen }) => GameScreens[gameScreen]}</GameContextConsumer>
--- packages/client/src/widgets/StartGameForm/ui/StartGameForm.tsx
+++ packages/client/src/widgets/StartGameForm/ui/StartGameForm.tsx
@@ -1,15 +1,7 @@
 import { Flex } from 'antd';
-import { GameContextInstance } from 'entities/GameContext';
 import { ChangeGameMode, SettingsButton, StartGameButton } from 'features/StartGameFields';
-import { useContext, useEffect } from 'react';
 
 export const StartGameForm = () => {
-  const { gameScreen } = useContext(GameContextInstance);
-
-  useEffect(() => {
-    console.log(gameScreen);
-  }, [gameScreen]);
-
   return (
     <Flex vertical align="center" justify="center" gap={32}>
       <ChangeGameMode />
