6ab751b9c669b8d41ecf6a5873b66287e01c3411
Damir Nasirov <krekki01@mail.ru>
2024-04-25 22:44:34 +0300

    feat: added hook to use timer & example how to use it

--- packages/client/src/widgets/GameCountdownForm/ui/GameCountdownForm.tsx
+++ packages/client/src/widgets/GameCountdownForm/ui/GameCountdownForm.tsx
@@ -1,14 +1,21 @@
 import { Flex, Typography } from 'antd';
+import { GameContextInstance } from 'entities/GameContext';
+import { useContext } from 'react';
+import { EGAME_SCREEN_VALUES } from 'shared/constants/game';
+import { useTimer } from 'shared/hooks';
 
 const { Text, Title } = Typography;
 
 export const GameCountdownForm = () => {
+  const { setGameScreen } = useContext(GameContextInstance);
+  const time = useTimer(7, () => setGameScreen(EGAME_SCREEN_VALUES.START_GAME));
+
   return (
     <Flex vertical align="center" justify="center" gap={16}>
       <Title level={5} className="title__primary">
         Игра начнётся через:
       </Title>
-      <Text className="numeric-font title__primary">15</Text>
+      <Text className="numeric-font title__primary">{time}</Text>
     </Flex>
   );
 };
