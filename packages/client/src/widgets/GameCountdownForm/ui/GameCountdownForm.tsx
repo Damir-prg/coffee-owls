fdd5b879161255aa8fbc618697586ed8182ac907
Damir Nasirov <krekki01@mail.ru>
2024-04-26 20:35:08 +0300

    refactor: changed custom useTimer on Countdown

--- packages/client/src/widgets/GameCountdownForm/ui/GameCountdownForm.tsx
+++ packages/client/src/widgets/GameCountdownForm/ui/GameCountdownForm.tsx
@@ -1,23 +1,27 @@
-import { Flex, Typography } from 'antd';
+import { Flex, Typography, Statistic } from 'antd';
 import { GameContextInstance } from 'entities/GameContext';
 import { useContext } from 'react';
 import { EGAME_SCREEN_VALUES } from 'shared/constants/game';
-import { useTimer } from 'shared/hooks';
 
+const { Countdown } = Statistic;
 const { Title } = Typography;
 
 export const GameCountdownForm = () => {
   const { setGameScreen } = useContext(GameContextInstance);
-  const time = useTimer(7, () => setGameScreen(EGAME_SCREEN_VALUES.START_GAME));
+
+  const deadline = Date.now() + 1000 * 7;
 
   return (
     <Flex vertical align="center" justify="center" gap={16}>
       <Title level={4} className="title__primary">
         Игра начнётся через:
       </Title>
-      <Title level={5} className="numeric-font title__primary">
-        {time}
-      </Title>
+      <Countdown
+        format="mm:ss"
+        value={deadline}
+        onFinish={() => setGameScreen(EGAME_SCREEN_VALUES.START_GAME)}
+        className="numeric-font title__primary"
+      />
     </Flex>
   );
 };
