fcb8dba208abee6b16d3c9b3f90522fd7471e5d5
Damir Nasirov <krekki01@mail.ru>
2024-04-26 20:11:04 +0300

    refactor: moved buttons into StartGameForm

--- packages/client/src/features/StartGameFields/index.ts
+++ packages/client/src/features/StartGameFields/index.ts
@@ -1,3 +1,1 @@
 export { ChangeGameMode } from './ui/ChangeGameMode';
-export { SettingsButton } from './ui/SettingsButton';
-export { StartGameButton } from './ui/StartGameButton';
--- packages/client/src/widgets/StartGameForm/ui/StartGameForm.tsx
+++ packages/client/src/widgets/StartGameForm/ui/StartGameForm.tsx
@@ -1,12 +1,27 @@
-import { Flex } from 'antd';
-import { ChangeGameMode, SettingsButton, StartGameButton } from 'features/StartGameFields';
+import { SearchOutlined } from '@ant-design/icons';
+import { Button, Flex } from 'antd';
+import { GameContextInstance } from 'entities/GameContext';
+import { ChangeGameMode } from 'features/StartGameFields';
+import { useContext } from 'react';
+import { EGAME_SCREEN_VALUES } from 'shared/constants/game';
+import '../styles/StartGameForm.css';
 
 export const StartGameForm = () => {
+  const { setGameScreen } = useContext(GameContextInstance);
+
   return (
     <Flex vertical align="center" justify="center" gap={32}>
       <ChangeGameMode />
-      <SettingsButton />
-      <StartGameButton />
+      <Button className="zero-border-radius" size="large" icon={<SearchOutlined />}>
+        Управление
+      </Button>
+      <Button
+        type="primary"
+        size="large"
+        className="game__start-button"
+        onClick={() => setGameScreen(EGAME_SCREEN_VALUES.TIMER)}>
+        НАЧАТЬ ИГРУ
+      </Button>
     </Flex>
   );
 };
