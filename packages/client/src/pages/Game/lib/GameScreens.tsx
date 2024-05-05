57a5e51cb631621bf0cbcdbfef08783c667ae745
Damir Nasirov <krekki01@mail.ru>
2024-05-05 19:16:55 +0300

    refactor: moved screen wrapper into screen list & renamed logs output

--- packages/client/src/pages/Game/lib/GameScreens.tsx
+++ packages/client/src/pages/Game/lib/GameScreens.tsx
@@ -2,9 +2,18 @@ import { ReactNode } from 'react';
 import { EGAME_SCREEN_VALUES } from 'shared/constants/game';
 import { GameCountdownForm } from 'widgets/GameCountdownForm';
 import { StartGameForm } from 'widgets/StartGameForm';
+import { GameContentContainer } from 'entities/GameContentContainer';
 
 export const GameScreens: Record<EGAME_SCREEN_VALUES, ReactNode> = {
-  [EGAME_SCREEN_VALUES.START_GAME]: <StartGameForm />,
-  [EGAME_SCREEN_VALUES.TIMER]: <GameCountdownForm />,
+  [EGAME_SCREEN_VALUES.START_GAME]: (
+    <GameContentContainer>
+      <StartGameForm />
+    </GameContentContainer>
+  ),
+  [EGAME_SCREEN_VALUES.TIMER]: (
+    <GameContentContainer>
+      <GameCountdownForm />
+    </GameContentContainer>
+  ),
   [EGAME_SCREEN_VALUES.SETTINGS]: <></>,
 };
--- packages/client/src/pages/Game/ui/ScreenSwitcher.tsx
+++ packages/client/src/pages/Game/ui/ScreenSwitcher.tsx
@@ -1,17 +1,12 @@
 import { GameContextConsumer, GameContextInstance } from 'entities/GameContext';
 import { GameScreens } from '../lib/GameScreens';
-import { GameContentContainer } from 'entities/GameContentContainer';
 import { useContext, useEffect } from 'react';
 
 export const ScreenSwitcher = () => {
   const { gameScreen } = useContext(GameContextInstance);
 
   useEffect(() => {
-    console.log(`Активная страница игры: ${gameScreen}`);
+    console.log(`Активный экран игры: ${gameScreen}`);
   }, [gameScreen]);
-  return (
-    <GameContentContainer>
-      <GameContextConsumer>{({ gameScreen }) => GameScreens[gameScreen]}</GameContextConsumer>
-    </GameContentContainer>
-  );
+  return <GameContextConsumer>{({ gameScreen }) => GameScreens[gameScreen]}</GameContextConsumer>;
 };
