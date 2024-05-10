0840ccefb7f9dfbc2e7de237e194184e97ddc1a7
Damir Nasirov <krekki01@mail.ru>
2024-05-10 13:57:24 +0300

    refactor: add type for gameModeTranslate

--- packages/client/src/shared/constants/game/types/ModeEnum.ts
+++ packages/client/src/shared/constants/game/types/ModeEnum.ts
@@ -3,7 +3,7 @@ export enum EGAME_MODE_VALUES {
   FREE_PLAY = 'free-play',
 }
 
-export const gameModeTranslate = {
+export const gameModeTranslate: Record<EGAME_MODE_VALUES, string> = {
   [EGAME_MODE_VALUES.TIME_LIMIT]: 'На время',
   [EGAME_MODE_VALUES.FREE_PLAY]: 'В своё удовольствие',
 };
