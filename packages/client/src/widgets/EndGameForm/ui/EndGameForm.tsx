2b5857ad44bcc747e6e59d757ad63867c57faac9
Damir Nasirov <krekki01@mail.ru>
2024-05-10 14:09:34 +0300

    refactor: moved GameContentContainer into screen components

--- packages/client/src/widgets/EndGameForm/ui/EndGameForm.tsx
+++ packages/client/src/widgets/EndGameForm/ui/EndGameForm.tsx
@@ -1,4 +1,5 @@
 import { Button, Flex, Typography } from 'antd';
+import { GameContentContainer } from 'entities/GameContentContainer';
 import { GameContextInstance } from 'entities/GameContext';
 import { useCallback, useContext } from 'react';
 import EROUTES from 'shared/RoutesEnum';
@@ -13,26 +14,28 @@ export const EndGameForm = () => {
   const handleStartNewGame = useCallback(() => setGameScreen(EGAME_SCREEN_VALUES.GAME), []);
 
   return (
-    <Flex vertical align="center" justify="center" gap={24}>
-      <Flex vertical align="center" justify="flex-start">
-        <Title level={3} className="title__primary">
-          Спасибо за игру!
-        </Title>
-        <Text className="text__center-align">К сожалению, больше нет ходов</Text>
+    <GameContentContainer>
+      <Flex vertical align="center" justify="center" gap={24}>
+        <Flex vertical align="center" justify="flex-start">
+          <Title level={3} className="title__primary">
+            Спасибо за игру!
+          </Title>
+          <Text className="text__center-align">К сожалению, больше нет ходов</Text>
+        </Flex>
+        <Flex vertical align="center" justify="center" gap={8}>
+          <Text className="text__center-align">
+            Вы можете ознакомиться с
+            <br />
+            <Link path={EROUTES.RATING} text="рейтингом&nbsp;участников" size="large" weight="normal" />
+            <br />
+            или
+          </Text>
+          <Button type="primary" size="large" className="full-width" onClick={handleStartNewGame}>
+            Повторить игру
+          </Button>
+          <Link path={EROUTES.HOME} text="Вернуться на главную страницу" size="small" weight="lighter" />
+        </Flex>
       </Flex>
-      <Flex vertical align="center" justify="center" gap={8}>
-        <Text className="text__center-align">
-          Вы можете ознакомиться с
-          <br />
-          <Link path={EROUTES.RATING} text="рейтингом&nbsp;участников" size="large" weight="normal" />
-          <br />
-          или
-        </Text>
-        <Button type="primary" size="large" className="full-width" onClick={handleStartNewGame}>
-          Повторить игру
-        </Button>
-        <Link path={EROUTES.HOME} text="Вернуться на главную страницу" size="small" weight="lighter" />
-      </Flex>
-    </Flex>
+    </GameContentContainer>
   );
 };
--- packages/client/src/widgets/StartGameForm/ui/StartGameForm.tsx
+++ packages/client/src/widgets/StartGameForm/ui/StartGameForm.tsx
@@ -5,23 +5,26 @@ import { ChangeGameMode } from 'features/StartGameFields';
 import { useContext } from 'react';
 import { EGAME_SCREEN_VALUES } from 'shared/constants/game';
 import '../styles/StartGameForm.css';
+import { GameContentContainer } from 'entities/GameContentContainer';
 
 export const StartGameForm = () => {
   const { setGameScreen } = useContext(GameContextInstance);
 
   return (
-    <Flex vertical align="center" justify="center" gap={32}>
-      <ChangeGameMode />
-      <Button className="game__start-button" size="large" icon={<SettingOutlined />}>
-        Настройки управление
-      </Button>
-      <Button
-        type="primary"
-        size="large"
-        className="game__start-button"
-        onClick={() => setGameScreen(EGAME_SCREEN_VALUES.TIMER)}>
-        НАЧАТЬ ИГРУ
-      </Button>
-    </Flex>
+    <GameContentContainer>
+      <Flex vertical align="center" justify="center" gap={32}>
+        <ChangeGameMode />
+        <Button className="game__start-button" size="large" icon={<SettingOutlined />}>
+          Настройки управление
+        </Button>
+        <Button
+          type="primary"
+          size="large"
+          className="game__start-button"
+          onClick={() => setGameScreen(EGAME_SCREEN_VALUES.TIMER)}>
+          НАЧАТЬ ИГРУ
+        </Button>
+      </Flex>
+    </GameContentContainer>
   );
 };
