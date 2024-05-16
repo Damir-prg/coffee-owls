0fe968db18a836b56f42d603cb0e8e0115b837b9
Damir Nasirov <krekki01@mail.ru>
2024-05-16 22:36:30 +0300

    feat: add time limit game

--- packages/client/src/widgets/GameCanvas/ui/GameCanvas.tsx
+++ packages/client/src/widgets/GameCanvas/ui/GameCanvas.tsx
@@ -1,10 +1,12 @@
-import { Button } from 'antd';
+import { Button, Statistic } from 'antd';
 import '../styles/GameCanvas.css';
 import { GameBoard } from './GameBoard';
 import { useCallback, useContext, useMemo, useState } from 'react';
 import { GameContextInstance } from 'entities/GameContext';
-import { EGAME_SCREEN_VALUES, gameModeTranslate } from 'shared/constants/game';
+import { EGAME_MODE_VALUES, EGAME_SCREEN_VALUES, gameModeTranslate } from 'shared/constants/game';
 import { Board } from '../lib/board';
+
+const { Countdown } = Statistic;
 
 export const GameCanvas = () => {
   const [score, setScore] = useState<number>(0);
@@ -13,6 +15,7 @@ export const GameCanvas = () => {
   const handleScore = useCallback((points: number) => setScore(score => score + points), [setScore]);
   const selectedGameMode = useMemo(() => gameModeTranslate[gameMode].toLowerCase(), [gameMode]);
   const handleClear = useCallback(() => setGameScreen(EGAME_SCREEN_VALUES.END_GAME), [setGameScreen]);
+  const deadline = useMemo(() => Date.now() + 1000 * 60, []);
 
   Board.setScoreHandler(handleScore);
   Board.setGameOverHandler(handleClear);
@@ -22,8 +25,11 @@ export const GameCanvas = () => {
       <GameBoard />
       <ul className="game__info">
         <li>Режим: {selectedGameMode}</li>
+        {gameMode === EGAME_MODE_VALUES.TIME_LIMIT && (
+          <Countdown format="mm:ss" value={deadline} onFinish={handleClear} />
+        )}
         <li>Счет: {score}</li>
-        <li>Лучший счет: 0</li>
+        <li>Лучший счет: {score}</li>
         <li>
           <Button size="large" onClick={handleClear}>
             Завершить игру
