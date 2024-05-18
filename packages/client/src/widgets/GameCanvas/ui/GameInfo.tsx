9324c5cd9a6fd9b7bfcb8ac7804a0cab023af453
Damir Nasirov <krekki01@mail.ru>
2024-05-18 13:27:28 +0300

    refactor: game mode values & board handlers

--- packages/client/src/widgets/GameCanvas/ui/GameInfo.tsx
+++ packages/client/src/widgets/GameCanvas/ui/GameInfo.tsx
@@ -1,18 +1,40 @@
-import { Alert, Button, List, Typography } from 'antd';
 import '../styles/GameInfo.css';
-import { useCallback, useContext, useMemo } from 'react';
+import { Alert, Button, List, Typography } from 'antd';
+import { useCallback, useContext, useEffect, useMemo, useState } from 'react';
+import { EGAME_MODE_VALUES, EGAME_SCREEN_VALUES, gameModeTranslate } from 'shared/constants/game';
 import { GameContextInstance } from 'entities/GameContext';
-import { EGAME_SCREEN_VALUES, gameModeTranslate } from 'shared/constants/game';
+import { Board } from '../lib/board';
+import { GameTimer } from './GameTimer';
 
 const HINT_TEXT = 'Для перехода в полноэкранный режим нажмите ENTER';
-const LIST_ITEMS_TITLE = ['Режим', 'Счет', 'Лучший счет'];
 
 export const GameInfo = () => {
+  const [score, setScore] = useState<number>(0);
   const { gameMode, setGameScreen } = useContext(GameContextInstance);
 
+  const handleScore = useCallback((points: number) => setScore((score: number) => score + points), [setScore]);
   const selectedGameMode = useMemo(() => gameModeTranslate[gameMode].toLowerCase(), [gameMode]);
-
   const handleFinishGame = useCallback(() => setGameScreen(EGAME_SCREEN_VALUES.END_GAME), [setGameScreen]);
+
+  const dataSource = useMemo(() => {
+    console.log(selectedGameMode);
+    if (gameMode === EGAME_MODE_VALUES.FREE_PLAY) {
+      return [selectedGameMode, score, score];
+    }
+    return [selectedGameMode, score, score, <GameTimer />];
+  }, [selectedGameMode, score, gameMode]);
+
+  const listTitles = useMemo(() => {
+    if (gameMode === EGAME_MODE_VALUES.FREE_PLAY) {
+      return ['Режим', 'Счет', 'Лучший счет'];
+    }
+    return ['Режим', 'Счет', 'Лучший счет', 'Время'];
+  }, [gameMode]);
+
+  useEffect(() => {
+    Board.setScoreHandler(handleScore);
+    Board.setGameOverHandler(handleFinishGame);
+  }, []);
 
   return (
     <>
@@ -24,10 +46,10 @@ export const GameInfo = () => {
             Завершить игру
           </Button>
         }
-        dataSource={[selectedGameMode, 0, 0]}
+        dataSource={dataSource}
         renderItem={(item, index) => (
           <List.Item>
-            <Typography.Text strong>{LIST_ITEMS_TITLE[index]}:</Typography.Text> {item}
+            <Typography.Text strong>{listTitles[index]}:</Typography.Text> {item}
           </List.Item>
         )}
         className="game__info"
