fc54d354d73d627c4e3a05cc94f7fbcbbd19668e
Olga <olgadimovaa@gmail.com>
2024-06-09 23:02:34 +0300

    fix: mock meta env variables for jest

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -7,6 +7,23 @@ export default {
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT || 3001,
+    'ts-jest': {
+      diagnostics: {
+        ignoreCodes: [1343],
+      },
+      astTransformers: {
+        before: [
+          {
+            path: 'ts-jest-mock-import-meta',
+            options: {
+              metaObjectReplacement: () => ({
+                env: { VITE_YANDEX_REDIRECT_URL: 'http://localhost:3000', PROD: false, DEV: true },
+              }),
+            },
+          },
+        ],
+      },
+    },
   },
   moduleNameMapper: {
     '\\.(css|svg|png|jpeg)$': 'identity-obj-proxy',
