1fb198c56c36092d9810157a0e388786e160bf25
Olga <olgadimovaa@gmail.com>
2024-06-21 20:34:18 +0300

    fix: app db connection and update vite server package version to match client

--- packages/server/db.ts
+++ packages/server/db.ts
@@ -1,12 +1,12 @@
 import { Client } from 'pg';
 
-const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT } = process.env;
+const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT, POSTGRES_HOST } = process.env;
 
 export const createClientAndConnect = async (): Promise<Client | null> => {
   try {
     const client = new Client({
       user: POSTGRES_USER,
-      host: 'localhost',
+      host: POSTGRES_HOST,
       database: POSTGRES_DB,
       password: POSTGRES_PASSWORD,
       port: Number(POSTGRES_PORT),
