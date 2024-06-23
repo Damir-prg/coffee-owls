94bae8f51116e0f9c3c961a9ff72882c297c8511
Damir Nasirov <krekki01@mail.ru>
2024-06-23 18:59:15 +0300

    feat: add forum api

---
+++ packages/client/src/shared/api/forumApi/forumApi.ts
@@ -0,0 +1,19 @@
+import api from '../api';
+import { ICreateComment, ICreateTopic, ITopicComment, ITopicDetails, ITopicItem } from './forumApi.interface';
+const forumUrl = 'http://localhost:3001/api';
+
+export const getTopics = (): Promise<Array<ITopicItem> | null> => {
+  return api.get(`${forumUrl}/topics`);
+};
+
+export const createTopic = (data: ICreateTopic): Promise<ITopicDetails | null> => {
+  return api.post(`${forumUrl}/topics`, { data });
+};
+
+export const getTopicById = (id: number): Promise<ITopicDetails | null> => {
+  return api.get(`${forumUrl}/topics/${id}`);
+};
+
+export const createComment = (id: number, data: ICreateComment): Promise<ITopicComment | null> => {
+  return api.post(`${forumUrl}/topics/${id}/comments`, { data });
+};
--- packages/client/src/shared/api/oauthApi/oauthApi.ts
+++ packages/client/src/shared/api/oauthApi/oauthApi.ts
@@ -1,7 +1,8 @@
+import { BaseUrlApi } from 'shared/config/config';
 import api from '../api';
 import { TYandexLoginRequest, TYandexService } from './oauth.models';
 
-const yandexAuthUrl = '/oauth/yandex';
+const yandexAuthUrl = BaseUrlApi + '/oauth/yandex';
 
 export const getYandexServiceId = (redirectUri: string): Promise<TYandexService | null> => {
   return api.get(`${yandexAuthUrl}/service-id?redirect_uri=${redirectUri}`);
