388cadb60274dac041e8dc345e99af0cbdf66463
Ivan Kostyulin <ivan-kostyulin@yandex.ru>
2024-07-09 00:16:43 +0300

    refactor: validation messages

--- packages/client/src/shared/lib/ValidationRules.ts
+++ packages/client/src/shared/lib/ValidationRules.ts
@@ -34,26 +34,26 @@ export const validatePasswordUpdate: RuleRender = ({ getFieldValue }) => ({
 export const validationRules = {
   firstName: {
     pattern: VALIDATION_PATTERNS.FIRST_NAME,
-    message: 'Неправильный формат имени!',
+    message: 'Первая буква должна быть заглавной, без цифр и символов',
   },
   secondName: {
     pattern: VALIDATION_PATTERNS.SECOND_NAME,
-    message: 'Неправильный формат фамилии!',
+    message: 'Первая буква должна быть заглавной, без цифр и символов',
   },
   email: {
     pattern: VALIDATION_PATTERNS.EMAIL,
-    message: 'Неправильный формат почты!',
+    message: 'Неправильный формат электронной почты',
   },
   phone: {
     pattern: VALIDATION_PATTERNS.PHONE,
-    message: 'Неправильный формат телефона!',
+    message: 'Должен состоять из цифр от 10 до 15 символов',
   },
   login: {
     pattern: VALIDATION_PATTERNS.LOGIN,
-    message: 'Неправильный формат логина!',
+    message: 'От 3 до 20 символов, латиница, может содержать цифры',
   },
   password: {
     pattern: VALIDATION_PATTERNS.PASSWORD,
-    message: 'Неправильный формат пароля!',
+    message: 'От 8 до 40 символов, обязательна одна заглавная буква и цифра',
   },
 };
