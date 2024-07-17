12cdf2363bec6eb178dc5bfa71e068e6634bde87
Ivan Kostyulin <ivan-kostyulin@yandex.ru>
2024-07-17 13:14:13 +0300

    fix: csp report-only

--- packages/server/middlewares/csp.middleware.ts
+++ packages/server/middlewares/csp.middleware.ts
@@ -4,7 +4,7 @@ const cspMiddleware = () => {
   return (_req: Request, res: Response, next: NextFunction) => {
     // git commit изменяет кавычки
     res.setHeader(
-      'Content-Security-Policy',
+      'Content-Security-Policy-Report-Only',
       // eslint-disable-next-line quotes
       "font-src 'self'; img-src 'self' blob: https://ya-praktikum.tech; frame-src 'self'; media-src 'self'",
     );
